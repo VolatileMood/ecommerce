4064bebf18cbb06a57d13ea551e96271ec2df8e6
Joey <jliew@tuta.io>
2020-04-17 18:39:01 +0200

    Add logic to login controller

--- server/src/controllers/users.js
+++ server/src/controllers/users.js
@@ -11,7 +11,7 @@ exports.register = async (req, res, next) => {
     if (usersEmail.length > 0) {
       return res.json({
         status: 'error',
-        data: {
+        message: {
           email: 'E-mail is already registered',
         },
       });
@@ -58,7 +58,49 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = async (req, res, next) => {};
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    // 1. Look for user with email.
+    const userEmail = await db('users').select().where({ email });
+    if (userEmail.length === 0) {
+      throw new AppError(401, 'Wrong Credentials.');
+    }
+    const user = userEmail[0];
+    // 2. Compare both passwords.
+    const valid = await bcrypt.compare(password, user.password);
+    if (!valid) {
+      throw new AppError(401, 'Wrong Credentials.');
+    }
+    // 3. Create access and refresh token.
+    const accessToken = createToken(
+      { tokenVersion: user.token_version, userId: user.id },
+      process.env.ACCESS_TOKEN_SECRET,
+      process.env.ACCESS_TOKEN_DURATION
+    );
+    const refreshToken = createToken(
+      { tokenVersion: user.token_version, userId: user.id },
+      process.env.REFRESH_TOKEN_SECRET,
+      process.env.REFRESH_TOKEN_DURATION
+    );
+    // 4. Attach refresh token to cookie.
+    res.cookie('rft', refreshToken, {
+      httpOnly: true,
+      maxAge: parseInt(process.env.REFRESH_TOKEN_DURATION),
+    });
+    // 5. Send user and tokens.
+    const { password: omit, ...rest } = user;
+    res.status(201).json({
+      status: 'success',
+      data: {
+        user: rest,
+        accessToken,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 exports.logout = async (req, res, next) => {};
 
